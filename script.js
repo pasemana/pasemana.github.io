a0b31bb175cddd266a221bf7d40ab3ad6b003713
Sivan <sipotat@gmail.com>
2025-02-02 21:14:12 +0000

    align buttons

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-  var downloadContainer = document.getElementById("downloadContainer");
+  var downloadContainer = document.getElementById("download-container");
 
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     downloadContainer.innerHTML =
-      '<a href="#" class="app-store"><img src="images/appstore.png" alt="App Store"/></a>';
+      '<a href="#" ><img src="images/appstore.png" alt="App Store"/></a>';
   } else if (/android/i.test(userAgent)) {
     downloadContainer.innerHTML =
       '<a href="#" class="google-play"><img src="images/googleplay.png" alt="Google Play"/></a>';
   } else {
     downloadContainer.innerHTML = `
-            <a href="#" class="app-store"><img src="images/appstore.png" alt="App Store"/></a>
-            <a href="#" class="google-play"><img src="images/googleplay.png" alt="Google Play"/></a>
+            <a href="#"><img src="images/appstore.png" alt="App Store"/></a>
+            <a href="#"><img src="images/googleplay.png" alt="Google Play"/></a>
         `;
   }
 });
