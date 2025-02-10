eb8029e25a58a90d73fbf9b58c97078c5c1d2b7b
Sivan <sipotat@gmail.com>
2025-02-10 20:42:31 +0000

    portugeese and everythingggggg

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     downloadContainer.innerHTML =
-      '<a href="#" ><img src="images/appstore.png" alt="App Store"/></a>';
+      '<a href="https://apps.apple.com/pt/app/yourappname/id6478970391" ><img src="images/appstore.png" alt="App Store"/></a>';
   } else if (/android/i.test(userAgent)) {
     downloadContainer.innerHTML =
-      '<a href="#" class="google-play"><img src="images/googleplay.png" alt="Google Play"/></a>';
+      '<a href="https://play.google.com/store/apps/details?id=com.pasemana.alentejo" class="google-play"><img src="images/googleplay.png" alt="Google Play"/></a>';
   } else {
     downloadContainer.innerHTML = `
-            <a href="#"><img src="images/appstore.png" alt="App Store"/></a>
-            <a href="#"><img src="images/googleplay.png" alt="Google Play"/></a>
+            <a href="https://apps.apple.com/pt/app/yourappname/id6478970391"><img src="images/appstore.png" alt="App Store"/></a>
+            <a href="https://play.google.com/store/apps/details?id=com.pasemana.alentejo"><img src="images/googleplay.png" alt="Google Play"/></a>
         `;
   }
 });
