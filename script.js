07ec64eb4edbfa8251b550bb917588edcdfda97f
Sivan Puterman <sipotat@gmail.com>
2025-04-12 15:03:39 +0200

    update app store link

--- script.js
+++ script.js
@@ -11,13 +11,13 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
     downloadContainer.innerHTML =
-      '<a href="https://apps.apple.com/pt/app/yourappname/id6478970391" class="app-store"><img src="images/appstore.png" alt="App Store"/></a>';
+      '<a href="https://apps.apple.com/pt/app/pa-semana/id6478970391" class="app-store"><img src="images/appstore.png" alt="App Store"/></a>';
   } else if (/android/i.test(userAgent)) {
     downloadContainer.innerHTML =
       '<a href="https://play.google.com/store/apps/details?id=com.pasemana.alentejo" class="google-play"><img src="images/googleplay.png" alt="Google Play"/></a>';
   } else {
     downloadContainer.innerHTML = `
-            <a href="https://apps.apple.com/pt/app/yourappname/id6478970391" class="app-store"><img src="images/appstore.png" alt="App Store"/></a>
+            <a href="https://apps.apple.com/pt/app/pa-semana/id6478970391" class="app-store"><img src="images/appstore.png" alt="App Store"/></a>
             <a href="https://play.google.com/store/apps/details?id=com.pasemana.alentejo" class="google-play"><img src="images/googleplay.png" alt="Google Play"/></a>
         `;
   }
